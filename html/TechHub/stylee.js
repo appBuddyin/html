add0306c71821db724c9870f62b814064128c5ab
nigam-chauhan <nigamchauhanmbd125@gmail.com>
2019-07-30 10:15:32 +0530

    html changes

---
+++ html/TechHub/stylee.js
@@ -0,0 +1,8 @@
+
+        function MyFunction(){
+            document.getElementById("demo").style.backgroundColor="yellow";
+            document.getElementById("demo").style.color="black";
+            document.getElementById("demo").style.fontFamily="verdana";
+        }
+        
+        
---
+++ javaScript/function.js
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <p id="demo">JavaScript Function</p>
+        <script>
+            var x = myFunction(3,4);
+            document.getElementById("demo").innerHTML=x;
+            Function myFunction(a,b){
+               return a + b;
+            }
+        </script>
+    </body>
+</html>
