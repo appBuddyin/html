45ce234d58eb2eefbcbf2bb79987c22f6609f8c0
nigam-chauhan <nigamchauhanmbd125@gmail.com>
2019-08-06 21:21:57 +0530

    JQHtml

---
+++ javaScript/jqTraverse.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $("#span1").parent().css({"color": "red", "border": "2px solid red"});
+    $("#div1").children().css({"color": "red", "border": "2px solid red"});
+  });
